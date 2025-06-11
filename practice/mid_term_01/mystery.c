d24318ea33b964468eccacaadc890b8ff7839a67
justineabucay <149826545+justineabucay@users.noreply.github.com>
2025-06-11 11:45:01 -0700

    Create mystery.c

---
+++ practice/mid_term_01/mystery.c
@@ -0,0 +1,13 @@
+// mystery.c
+
+// function takes in an array of 8 integers and cells (i.e., number of integers in arr) 
+// stuff[LENGTH=10]
+
+// for loop: length limit (or cells?)
+// if not even
+    // stuff[arr[i]]+*
+// else 
+    // stuff[arr[i]]+=2
+    
+// now, that im thinking about it, there's an error b/c arr has a size of only 8 integers 
+// unless i did not remember correctly and the loop limit is set to the size of arr
