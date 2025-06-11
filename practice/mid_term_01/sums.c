b953c9cf60fc0a75637659339576c42f7fef8f1f
Justine Abucay <justine_abucay@sfu.ca>
2025-06-11 14:58:39 -0700

    adding new files

--- practice/mid_term_01/sums.c
+++ practice/mid_term_01/sums.c
@@ -1,10 +1,50 @@
 // float user input -- one at a time 
 // sum and smallest - functions 
 
+#include <stdio.h>
+
+
+int getSmallest(int num)
+{
+    int smallest = 0; 
+    if(num < smallest ) smallest = num; 
+
+    return num;
+}
+
 int main(void)
 {
     float const MIN = -100.0;
     float const MAX = 100.0;
+
+    float input = 0.0; 
+    int result = 0; 
+
+    float sum = 0;
+    float smallest = 0; 
+
+    printf("Please, enter float integers from %f to %f: ", MIN, MAX); 
+    
+    while((result = scanf("%f ", &input)) != -1)
+    {
+        if(input <= MIN && input >= MAX)
+        {
+            printf("\nyou have entered an invalid number. please, try again: ");
+        }
+        else 
+        {
+            sum += input;
+            smallest = getSmallest(input);
+        }
+    }
+
+    printf("All inputs are successful. Now, printing results!\n");
+    
+    printf("Sum: %.2f\n", sum);
+    printf("Smallest: %.2f\n", smallest);
+    
+    
+
     
     
 }
